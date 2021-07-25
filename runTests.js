7502cdd2b4fd19cc1944cf84b0f26931b14e389e
gregoriB <brandon.gregori@gmail.com>
2021-07-24 23:07:04 -0400

    Clean up tests, add new test for transaction

--- runTests.js
+++ runTests.js
@@ -1,7 +1,10 @@
 //TODO: Make fancy recursive function that gets test files by the "test" in the file name
 const TestSuite = require('./lib/TestSuite');
+const transactionTestResults = require('./lib/tests/transaction.test');
 const blockTestResults = require('./lib/tests/block.test');
 const blockchainTestResults = require('./lib/tests/blockchain.test');
 
-new TestSuite().logTestResults(blockTestResults);
-new TestSuite().logTestResults(blockchainTestResults);
+const testSuite = new TestSuite();
+const results = [transactionTestResults, blockTestResults, blockchainTestResults];
+
+testSuite.logTestResults(results);
